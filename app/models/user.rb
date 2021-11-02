23bb3c59a4a0eeb4bff4c07ff75472257903b8cb
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-02 23:21:30 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User < ApplicationRecord
   validates :encrypted_password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}, presence: true
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true 
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_furigana, presence: true
-  validates :family_furigana, presence: true
+  validates :first_name, presence: true, format:{with: /\A[一-龥ぁ-ん]/}
+  validates :family_name, presence: true, format:{with: /\A[一-龥ぁ-ん]/}
+  validates :first_furigana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
+  validates :family_furigana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 
 end
