58f84d589c33fbb2d347eccd938f42c9c343f808
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-02 17:31:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+
+  # バリデーションの設定
+  validates :nickname, presence: true
+  validates :encrypted_password, presence: true
+  validates :email, presence: true, uniqueness: true 
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_furigana, presence: true
+  validates :family_furigana, presence: true
+  validates :birthday, presence: true
+
 end
