54cbaa3bbeee7bdc5f9d308657a0c83440484f47
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-02 21:45:43 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:encrypted_password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_furigana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_furigana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
