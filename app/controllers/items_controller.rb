ce23603d084fca8b48d6e9bce9eec0be487f69de
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-04 10:57:06 +0900

    ルーティング・バリデーションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :image, :explain, :category_id, :detail_id, :change_id, :prefecture_id, :shipp_id, :price )
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one :purchase
+
+  # バリデーションの設定
+  validates :item_name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :detail_id, presence: true
+  validates :change_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipp_id, presence: true
+  validates :price, presence: true
+
 end
