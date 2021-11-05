ec51bb362e33369b8ae43fa9056a152496a04ea2
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-05 13:18:53 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
+    item_name              {'testテストてすと1234１２３４'}
+    explain                {'testテストてすと1234１２３４'}
+    category_id            {'2'}
+    detail_id              {'2'}
+    charge_id              {'2'}
+    prefecture_id          {'2'}
+    shipp_id               {'2'}
+    price                  {'300'}
+    # image                  {Faker::Lorem.sentence}
+
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
