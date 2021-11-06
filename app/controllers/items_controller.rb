c14683d713744704a0c1298d7fe2315b13ad1ef4
Dekopon06 <92018465+Dekopon06@users.noreply.github.com>
2021-11-06 17:35:26 +0900

    ユーザーのアクセス制限に関する記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new ]
+  before_action :authenticate_user!, only: [ :new, :edit ]
 
   def index
     @items = Item.order("created_at DESC")
@@ -21,6 +21,27 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+
+   unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+   end
+
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+       redirect_to root_path
+    else
+       render :edit
+    end
+
+  end 
+
 
   # def destroy
   # end
